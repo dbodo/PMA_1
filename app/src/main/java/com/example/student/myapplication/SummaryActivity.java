ffa4a41e3db47f5f052e70173a4ec8bdca2cb144
Domagoj Bodo <domagoj.bodo@vsmti.hr>
2018-11-08 15:54:50 +0100

    Small changes

--- app/src/main/java/com/example/student/myapplication/SummaryActivity.java
+++ app/src/main/java/com/example/student/myapplication/SummaryActivity.java
@@ -1,13 +1,35 @@
 package com.example.student.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Button;
+
 
 public class SummaryActivity extends AppCompatActivity {
-
+    private Button oVrati;
+    private String sImePrezime;
+    private String sPredmet;
+    private TextView oImePrezime;
+    private TextView oPredmet;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_summary);
+        final Bundle oExtras = getIntent().getExtras();
+        sImePrezime = oExtras.getString("imePrezime");
+        sPredmet = oExtras.getString("predmet");
+        oImePrezime = (TextView)findViewById(R.id.tvImePrezimeUpis);
+        oPredmet = (TextView)findViewById(R.id.tvPredmetUpis);
+        oVrati = (Button)findViewById(R.id.button3);
+        oVrati.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent oVratiNaPocetnu = new Intent(getApplicationContext(), PersonalInfoActivity.class);
+                startActivity(oVratiNaPocetnu);
+            }
+        });
     }
 }
